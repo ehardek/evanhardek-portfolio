34efe5fb1e6e6dd5bb16c4361f2d244204b9d118
Evan Hardek <evanhardek@gmail.com>
2021-08-07 21:55:09 -0400

    Began the implementation of the react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import React from 'react';
-import Header from './components/Header'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+
 function App() {
   return (
-    <div className="App">
-     <Header/>
-     
-    </div>
+    <Router>
+      <>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={About} />
+          <Route exact path='/contactEvan' component={Contact} />
+          <Route exact path='/myProjects' component={Projects} />
+          <Route render={() => <h1>Woah, How did you get here man?</h1>} />
+        </Switch>
+        <Footer/>
+      </>
+    </Router>
   );
 }
-
 export default App;
